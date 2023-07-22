4c3403764fd9ac8de7d9248457c7d88726291623
2112tamanna <j.tamanna@iitg.ac.in>
2023-07-22 18:56:59 +0530

    Deployment

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -27,7 +27,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    gidef initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
